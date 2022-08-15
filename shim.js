0a2b94400fc033231083c864534bb44d63ef82b2
Jordan Harband <ljharb@gmail.com>
2022-08-14 21:49:51 -0700

    [New] `shim`/`auto`: add `with` to `Symbol.unscopables`

--- shim.js
+++ shim.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var define = require('define-properties');
+var shimUnscopables = require('es-shim-unscopables');
+
 var getPolyfill = require('./polyfill');
 
 module.exports = function shim() {
 	var polyfill = getPolyfill();
+
 	define(
 		Array.prototype,
 		{ 'with': polyfill },
 		{ 'with': function () { return Array.prototype['with'] !== polyfill; } }
 	);
+
+	shimUnscopables('with');
+
 	return polyfill;
 };
